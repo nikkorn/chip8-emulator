11315982d1d539023f1e010a319210db08aa299a
nikkorn <nikhoward1234@hotmail.co.uk>
2020-04-17 16:10:34 +0100

    added opcodes

--- src/chip8_emulator/Machine.java
+++ src/chip8_emulator/Machine.java
@@ -21,18 +21,27 @@ public class Machine {
 	 */
 	private short I = 0;
 	/**
+	 * The stack and stack pointer used for tracking calls through subroutines.
+	 */
+	private short[] stack = new short[16];
+	private short SP = 0;
+	/**
 	 * The pixel values for the display.
 	 * White if true, otherwise black.
 	 */
 	private boolean[] pixels = new boolean[Constants.DISPLAY_WIDTH * Constants.DISPLAY_HEIGHT];
 	/**
-	 * Whether the display has changed as a side-effect of exectuing the last opcode.
+	 * Whether the display has changed as a side-effect of executing the last opcode.
 	 */
 	private boolean hasDisplayChanged = false;
 	/**
 	 * The ROM data.
 	 */
 	private byte[] rom;
+	/**
+	 * The input key states.
+	 */
+	private boolean[] keys = new boolean[16];
 	
 	/**
 	 * Sets the rom data.
@@ -41,7 +50,7 @@ public class Machine {
 	public void setRomData(byte[] rom) {
 		this.rom = rom;
 		
-		// Clear the portion of memeory reserved for rom data.
+		// Clear the portion of memory reserved for rom data.
 		for (int index = 0x200; index <= 0xFFF; index++) {
 			memory[index] = 0;
 		}
@@ -69,6 +78,9 @@ public class Machine {
 			 */
 			case 0x0000:
 				switch (opcode & 0x000F) {
+					/**
+					 * Clear the screen.
+					 */
 					case 0x0000:
 						// Clear the screen.
 						pixels = new boolean[Constants.DISPLAY_WIDTH * Constants.DISPLAY_HEIGHT];
@@ -77,8 +89,13 @@ public class Machine {
 						hasDisplayChanged = true;
 						break;
 						
+					/**
+					 * Return from the current subroutine.
+					 */
 					case 0x000E:
-						// TODO Return from subroutine.
+						// Set the program counter to the last counter position we were at.
+						PC = stack[--SP];
+						PC += 2;
 						break;
 						
 					default:
@@ -90,6 +107,17 @@ public class Machine {
 			 * (0x1NNN) Jump to address NNN.
 			 */
 			case 0x1000:
+				PC = (short) (opcode & 0x0FFF);
+				break;
+				
+			/**
+			 * (0x2NNN) Calls subroutine at NNN.
+			 */
+			 case 0x2000:
+				// Add the current program counter to the stack.
+				stack[SP++] = PC;
+				
+				// Set the program counter to the address defined by the opcode.
 				PC = (short) (opcode & 0x0FFF);
 				break;
 				
@@ -117,6 +145,24 @@ public class Machine {
 				break;
 				
 			/**
+			 * (0x8000) Various register operations.
+			 */
+			 case 0x8000:
+				switch (opcode & 0x000F) {
+                   /**
+				    * (0x8XY0) Sets register X to the value of register Y.
+				    */
+				   case 0x0000:
+					   registers[(opcode & 0x0F00) >> 8] = registers[(opcode & 0x00F0) >> 4];
+					   PC += 2;
+					   break;
+				                  
+				   default:
+					   throw new RuntimeException("unknown opcode: 0x" + opcode);
+				}
+				break;
+				
+			/**
 			 * (0xANNN) Sets I to the address NNN.
 			 */
 			case 0xA000:
@@ -128,10 +174,6 @@ public class Machine {
 			 * (0xDXYN) Draws a sprite at coordinate (register X, register Y) that has a width of 8 pixels and a height of N.
 			 */
 			case 0xD000:
-				// TODO Do this for real!
-				System.out.println("draw!");
-			
-				
 				
 				// TODO Replace this stolen code.
 				short x = registers[(opcode & 0x0F00) >> 8];
@@ -154,15 +196,36 @@ public class Machine {
 				}
 				
 				
-				
 				// We will need to redraw the display.
 				hasDisplayChanged = true;
 				PC += 2;
 				break;
-		
 				
-				
-				
+			/**
+			 * (0xE000) Various key operations.
+			 */
+			case 0xE000:
+				switch (opcode & 0x000F) {
+					/**
+					 * (0xEX01) Skips the next instruction if the key stored in register X is not pressed.
+					 */
+					case 0x0001:
+						// TODO
+						PC += 4;
+						break;
+						
+					/**
+					 * (0xEX0E) Skips the next instruction if the key stored in register X is pressed.
+					 */
+					case 0x000E:
+						// TODO
+						PC += 2;
+						break;
+						
+					default:
+						throw new RuntimeException("unknown opcode: 0x" + opcode);
+				}
+				break;
 			
 			/**
 			 * (0xF000) Various register operations.
